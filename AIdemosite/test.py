3c6b5f34f2ef9c1cab92b4d3626efaf389e36c89
Alan Tao (Manpower Services(Taiwan)Co <a-alantao@microsoft.com>
2022-10-19 11:25:11 +0800

    disabled test.py

--- AIdemosite/test.py
+++ AIdemosite/test.py
@@ -1,31 +1,31 @@
-import os
-from azure.keyvault.secrets import SecretClient
-from azure.identity import DefaultAzureCredential
+# import os
+# from azure.keyvault.secrets import SecretClient
+# from azure.identity import DefaultAzureCredential
 
-keyVaultName = os.environ["KEY_VAULT_NAME"]
-print(keyVaultName)
-KVUri = f"https://{keyVaultName}.vault.azure.net"
+# keyVaultName = os.environ["KEY_VAULT_NAME"]
+# print(keyVaultName)
+# KVUri = f"https://{keyVaultName}.vault.azure.net"
 
-credential = DefaultAzureCredential()
-client = SecretClient(vault_url=KVUri, credential=credential)
+# credential = DefaultAzureCredential()
+# client = SecretClient(vault_url=KVUri, credential=credential)
 
-secretName = 'REGION'
-secretValue = 'eastasia'
+# secretName = 'REGION'
+# secretValue = 'eastasia'
 
-print(f"Creating a secret in {keyVaultName} called '{secretName}' with the value '{secretValue}' ...")
+# print(f"Creating a secret in {keyVaultName} called '{secretName}' with the value '{secretValue}' ...")
 
-client.set_secret(secretName, secretValue)
+# client.set_secret(secretName, secretValue)
 
-print(" done.")
+# print(" done.")
 
-print(f"Retrieving your secret from {keyVaultName}.")
+# print(f"Retrieving your secret from {keyVaultName}.")
 
-retrieved_secret = client.get_secret(secretName)
+# retrieved_secret = client.get_secret(secretName)
 
-print(f"Your secret is '{retrieved_secret.value}'.")
-# print(f"Deleting your secret from {keyVaultName} ...")
+# print(f"Your secret is '{retrieved_secret.value}'.")
+# # print(f"Deleting your secret from {keyVaultName} ...")
 
-# poller = client.begin_delete_secret(secretName)
-# deleted_secret = poller.result()
+# # poller = client.begin_delete_secret(secretName)
+# # deleted_secret = poller.result()
 
-print(" done.")
+# print(" done.")
