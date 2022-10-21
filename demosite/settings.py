0ecb2292e606b79520db574df444f645d665f847
Alan Tao (Manpower Services(Taiwan)Co <a-alantao@microsoft.com>
2022-10-21 17:31:40 +0800

    bug fix

--- demosite/settings.py
+++ demosite/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# MEDIA_URL = '/images/'
-MEDIA_URL = ''
+MEDIA_URL = '/images/'
+# MEDIA_URL = ''
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
