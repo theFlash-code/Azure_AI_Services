cd65c79142c7ea387dbb0a6dfbe741f9a1abc326
Alan Tao (Manpower Services(Taiwan)Co <a-alantao@microsoft.com>
2022-11-02 16:57:56 +0800

    Speech Service test with multi language

--- static/js/speech_service.js
+++ static/js/speech_service.js
@@ -1,5 +1,5 @@
-var phraseDiv;
-var startRecognizeOnceAsyncButton;
+var text_result;
+var btn_s2t;
 
 // subscription key and region for speech services.
 var subscriptionKey = "ac1b8fe3034344a787dc45b06243f51b",
@@ -8,18 +8,16 @@ var SpeechSDK;
 var recognizer;
 
 document.addEventListener("DOMContentLoaded", function () {
-  startRecognizeOnceAsyncButton = document.getElementById(
-    "startRecognizeOnceAsyncButton"
-  );
+  btn_s2t = document.getElementById("btn_s2t");
   //   subscriptionKey = document.getElementById("subscriptionKey");
   //   serviceRegion = document.getElementById("serviceRegion");
   subscriptionKey = "ac1b8fe3034344a787dc45b06243f51b";
   serviceRegion = "eastasia";
-  phraseDiv = document.getElementById("phraseDiv");
+  text_result = document.getElementById("result");
 
-  startRecognizeOnceAsyncButton.addEventListener("click", function () {
-    startRecognizeOnceAsyncButton.disabled = true;
-    phraseDiv.innerHTML = "";
+  btn_s2t.addEventListener("click", function () {
+    btn_s2t.disabled = true;
+    text_result.innerHTML = "";
 
     if (subscriptionKey === "" || subscriptionKey === "subscription") {
       alert(
@@ -31,23 +29,23 @@ document.addEventListener("DOMContentLoaded", function () {
       subscriptionKey,
       serviceRegion
     );
-
-    speechConfig.speechRecognitionLanguage = "en-US";
+    var lang_slct = document.getElementById("lang_slct");
+    speechConfig.speechRecognitionLanguage = lang_slct.value;
     var audioConfig = SpeechSDK.AudioConfig.fromDefaultMicrophoneInput();
     recognizer = new SpeechSDK.SpeechRecognizer(speechConfig, audioConfig);
 
     recognizer.recognizeOnceAsync(
       function (result) {
-        startRecognizeOnceAsyncButton.disabled = false;
-        phraseDiv.innerHTML += result.text;
+        btn_s2t.disabled = false;
+        text_result.innerHTML += result.text;
         window.console.log(result);
 
         recognizer.close();
         recognizer = undefined;
       },
       function (err) {
-        startRecognizeOnceAsyncButton.disabled = false;
-        phraseDiv.innerHTML += err;
+        btn_s2t.disabled = false;
+        text_result.innerHTML += err;
         window.console.log(err);
 
         recognizer.close();
@@ -58,9 +56,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   if (!!window.SpeechSDK) {
     SpeechSDK = window.SpeechSDK;
-    startRecognizeOnceAsyncButton.disabled = false;
+    btn_s2t.disabled = false;
 
-    document.getElementById("content").style.display = "block";
-    document.getElementById("warning").style.display = "none";
+    // document.getElementById("content").style.display = "block";
+    // document.getElementById("warning").style.display = "none";
   }
 });
